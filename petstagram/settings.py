a6acdfae928745508c23007b7ae9a313db367cc3
Doncho Minkov <DonchoMinkov@gmail.com>
2021-08-09 20:55:35 +0300

    Added STATIC_ROOM

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -135,8 +135,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
